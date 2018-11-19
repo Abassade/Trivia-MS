075cf1a65c433732549064c129a4a5d1fcfe0969
Abass <abs4real16.ma@gmail.com>
2018-11-19 13:44:29 +0100

    added comment and changed the port number

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT ||3000;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// connecting to mongodb 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Questiondb',{ useNewUrlParser: true }); 
 
+// using bodyparser to accept request body in json and url
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// using route in the  entry file
 const route = require('./routes/route');
 route(app);
 
+// set app to listen for a specified port
 app.listen(port, ()=>{
     console.log('The app is runnning on '+port);
 });
 
+// use the function to feedback the error status
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
   });
--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,13 @@
 module.exports = function(app) {
     const controller = require('../controllers/controller');
   
-    
+  // endpoint for non params body  
     app.route('/question')
       .get(controller.list_all_questions)
       .post(controller.create_a_question)
       .delete(controller.delete_all);
   
+      // endpoint for params body  
     app.route('/question/:id')
       .get(controller.read_a_question)
       .put(controller.update_a_question)
